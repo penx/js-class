7b0c4ed8da0f700f7792fb36fdb42197a7522c17
Alasdair McLeay <alasdair.mcleay@me.com>
2016-04-20 10:35:48 +0100

    destructuring complete

--- exercises/destructuring/exercise.js
+++ exercises/destructuring/exercise.js
@@ -8,6 +8,9 @@ let down = 'Down!';
 // TODO make Alice drink a potion - switch up and down in one line!
 // this is tricky - perhaps you could destructure an array some how?
 
+({ up: down, down: up } = { up, down });
+
+//[down,up] = [up,down]
 
 export { up, down };
 
@@ -19,6 +22,7 @@ let winner;
 let others;
 
 // TODO destruture the winner into winner, and the rest into others
+[winner, ...others] = raceParticipants;
 
 
 export { winner as raceWinner, others as raceOthers };
@@ -32,7 +36,7 @@ const guest = { name: 'Hare', treat: 'chocolate eggs' };
 let guestName;
 
 // TODO destruture the name into guestName in one line
-
+({ name: guestName } =  guest);
 
 export { guestName };
 
